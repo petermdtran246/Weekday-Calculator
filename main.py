83f2ee8cf445c3216ff0ec3706c2e17493f0d521
petermdtran246 <petermdtran246@gmail.com>
2024-05-25 17:25:11 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def DayName():
+    day = int(input('Enter a day number: '))
+    if day == 1:
+        print('sun')
+    elif day == 2:
+        print('mon')
+    elif day == 3:
+        print('tue')
+    elif day == 4:
+        print('wed')
+    elif day == 5:
+        print('thu')
+    elif day == 6:
+        print('fri')
+    elif day == 7:
+        print('sat')
+
+DayName()
